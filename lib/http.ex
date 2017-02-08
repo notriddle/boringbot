f7e2cc075b955b79d910670d96b66174cf4438e2
Michael Howell <michael@notriddle.com>
2017-02-08 10:00:17 -0700

    Ping about pull requests, too

--- lib/http.ex
+++ lib/http.ex
@@ -9,22 +9,29 @@ defmodule Boringbot.Http do
   plug :dispatch
 
   post "/webhook/github" do
-    case get_req_header(conn, "x-github-event") do
+    send_issue = case get_req_header(conn, "x-github-event") do
       ["issues"] ->
-        run = case conn.body_params["action"] do
-          "opened" -> true
-          "reopened" -> true
-          "closed" -> true
-          _ -> false
+        case conn.body_params["action"] do
+          "opened" -> conn.body_params["issue"]
+          "reopened" -> conn.body_params["issue"]
+          "closed" -> conn.body_params["issue"]
+          _ -> nil
         end
-        :ok = if run do
-          GenServer.call(
-            @bot,
-            {:webhook, :issue, conn.body_params["issue"]})
-        else
-          :ok
+      ["pull_request"] ->
+        pull_request = conn.body_params["pull_request"]
+        issue = Map.put(pull_request, "pull_request", pull_request)
+        case conn.body_params["action"] do
+          "opened" -> issue
+          "reopened" -> issue
+          "closed" -> issue
+          _ -> nil
         end
-      _ -> :ok
+      _ ->
+        nil
+    end
+
+    unless is_nil send_issue do
+      :ok = GenServer.call(@bot, {:webhook, :issue, send_issue})
     end
 
     send_resp(conn, 200, "")
