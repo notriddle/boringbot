4369b4c0608a51f724eac43906e9e0660b1693d3
Michael Howell <michael@notriddle.com>
2017-02-07 18:21:39 -0700

    ERTS has to be included to hot upgrade

--- rel/config.exs
+++ rel/config.exs
@@ -28,7 +28,7 @@ environment :dev do
 end
 
 environment :prod do
-  set include_erts: false
+  set include_erts: true
   set include_src: false
   set cookie: :"jt[lTO>(O3)YgjnrO~nd~{0N%b2V<o9OJGF~~f087igwOp3mdlQ7Egn&A=5@Cvt{"
 end
