28657b520c55866c0f0659546cabdf18a41e7848
Michael Howell <michael@notriddle.com>
2017-02-07 18:11:08 -0700

    Increment version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Boringbot.Mixfile do
 
   def project do
     [app: :boringbot,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
